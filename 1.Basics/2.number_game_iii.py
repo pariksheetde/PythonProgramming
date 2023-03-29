ddaacfad9aa54dc19070fa0fc4ddf0d9af147f53
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-29 22:45:10 +0530

    commit

--- 1.Basics/2.number_game_iii.py
+++ 1.Basics/2.number_game_iii.py
@@ -5,5 +5,9 @@ integer_division_i = 13 // 5
 print(f"The result of integer division : {integer_division_i}")
 
 # Modulus operator returns only the remainder
-remainder = 13 % 5
-print(f"The remainder is : {remainder}")
+remainder_case_i = 13 % 5
+print(f"The remainder is : {remainder_case_i}")
+
+# Modulus operator returns only the remainder
+remainder_case_ii = 10 % 5
+print(f"The remainder is : {remainder_case_ii}")
