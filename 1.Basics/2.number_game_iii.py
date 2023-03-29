7e796791c10faeaff6315be17405a19a9ba04f58
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-29 22:28:17 +0530

    commit

--- 1.Basics/2.number_game_iii.py
+++ 1.Basics/2.number_game_iii.py
@@ -1,13 +1,9 @@
 age = 30 # integer type
 PI = 3.14159 # float type
-maths_operation = ((1 + 3 + 4) / 2) - 2
-print(f"The result of addition {maths_operation}") # returns float type
 
-float_division = 12 / 5
-print(f"The result of float division : {float_division}") # returns float
+integer_division_i = 13 // 5
+print(integer_division_i)
 
-integer_division_i = 12 // 5
-print(f"The result of 1st integer division : {integer_division_i}") # returns integer
-
-integer_division_ii = 40 // 3
-print(f"The result of 2nd integer division : {integer_division_ii}")
+# Modulus operator returns only the remainder
+integer_division_ii = 13 % 5
+print(integer_division_ii)
