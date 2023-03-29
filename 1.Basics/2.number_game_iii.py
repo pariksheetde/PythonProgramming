3147900a225dff43735af61698cc13d6e5722349
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-29 22:29:21 +0530

    commit

--- 1.Basics/2.number_game_iii.py
+++ 1.Basics/2.number_game_iii.py
@@ -2,8 +2,8 @@ age = 30 # integer type
 PI = 3.14159 # float type
 
 integer_division_i = 13 // 5
-print(integer_division_i)
+print(f"The result of integer division : {integer_division_i}")
 
 # Modulus operator returns only the remainder
-integer_division_ii = 13 % 5
-print(integer_division_ii)
+remainder = 13 % 5
+print(f"The remainder is : {remainder}")
