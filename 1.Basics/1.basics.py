58322c4a72b95b7b16548b3f5c6162a3f5039ad7
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-29 22:15:02 +0530

    commit

---
+++ 1.Basics/1.basics.py
@@ -0,0 +1,3 @@
+age = 30
+name = "Pariksheet"
+print(f"My name is {name} and I am {age} years old")
---
+++ 1.Basics/2.number_game_ii.py
@@ -0,0 +1,10 @@
+age = 30 # integer type
+PI = 3.14159 # float type
+maths_operation = ((1 + 3 + 4) / 2) - 2
+print(f"The result of addition {maths_operation}") # returns float type
+
+float_division = 12 / 5
+print(f"The result of float division : {float_division}") # returns float
+
+integer_division = 12 // 5
+print(f"The result of integer division : {integer_division}") # returns integer
