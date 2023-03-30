baf8461e024d3c3b39f4fb99be7d4f887ded9ea9
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-30 09:43:37 +0530

    commit

---
+++ 1.Basics/3.string_formatting_ii.py
@@ -0,0 +1,17 @@
+string_with_quotes = "Hey! it's birthday"
+print(string_with_quotes)
+
+another_with_quotes = 'Let"s have a party tonight'
+print(another_with_quotes)
+
+stmt = """My name is Pariksheet \
+    I am born in India Kolkata
+"""
+print(f"About myself : {stmt}")
+
+age = 30
+name = "Pariksheet"
+print(f"My name is {name} and I am {age} years old")
+
+marks = 74
+print("I got " + format(74) + " in exam")
---
+++ 1.Basics/3.string_formatting_iii.py
@@ -0,0 +1,15 @@
+GREETING = "How are you doing {}"
+
+name = "Monica"
+final_greeting = GREETING.format(name)
+print(final_greeting)
+
+name = "Kate"
+final_greeting = GREETING.format(name)
+print(final_greeting)
+
+name = "Pariksheet"
+activity = "Learning Python Programming"
+
+stmt = "My name is {}"
+print(stmt.format(name))
