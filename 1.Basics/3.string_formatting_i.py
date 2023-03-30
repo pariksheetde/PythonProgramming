488fb5bb913e958c6ca8086d3334f006fa3e718f
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-03-30 09:16:53 +0530

    commit

---
+++ 1.Basics/3.string_formatting_i.py
@@ -0,0 +1,10 @@
+string_with_quotes = "Hey! it's birthday"
+print(string_with_quotes)
+
+another_with_quotes = 'Let"s have a party tonight'
+print(another_with_quotes)
+
+stmt = """My name is Pariksheet \
+    I am born in India Kolkata
+"""
+print(f"About myself : {stmt}")
