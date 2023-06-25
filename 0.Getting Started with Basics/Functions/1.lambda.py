6fd4c45df7eea96bc6fb3f1f1d9de920a432c914
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-06-25 13:39:15 +0530

    --COMMIT -lambda.py

--- 0.Getting Started with Basics/Functions/1.lambda.py
+++ 0.Getting Started with Basics/Functions/1.lambda.py
@@ -1,7 +1,10 @@
 '''
 lambda expression
+A lambda function is a small anonymous function. Python Lambda Functions are anonymous
+function means that the function is without a name.
 '''
 fn_1 = lambda x: x + 2
+print(fn_1)
 print(fn_1(2))
 
 print("-------------------------------------------------------------------------------EOL-----------------------------------------------------------------------------")
