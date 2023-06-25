50ef053b45bbf62d11327534552f2fcb4c8a1d58
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-06-25 12:17:30 +0530

    commit

---
+++ 0.Getting Started with Basics/Conditions/0.if.py
@@ -0,0 +1,22 @@
+age = 11
+
+if age < 10:
+    print(f'{age} is less than 10')
+if age > 10 :
+    print(f'{age} is greaten than 10')
+if age == 10:
+    print(f'{age} is same as 10')
+    
+print("--------------------------------------------------------EOL--------------------------------------------------------")
+
+    
+age = 11
+
+if age < 5:
+    print(f'{age} is less than 5')
+elif age > 5:
+    print(f'{age} is greater than 5')
+elif age <= 5:
+    print(f'{age} is same as 5')
+    
+print("--------------------------------------------------------EOL--------------------------------------------------------")
