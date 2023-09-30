817059755c35de04edb6b2a4bc3608fc00d4c029
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-09-30 19:37:47 +0530

    --COMMIT

--- 2.OracleDB/0.load_locations.py
+++ 2.OracleDB/0.load_locations.py
@@ -4,7 +4,7 @@ print(type(cx_Oracle))
 
 try:
     # create Oracle DB connection
-    connection = cx_Oracle.connect("Hr/Hr@localhost:1521/prod")
+    connection = cx_Oracle.connect("Hr/Deepsi@123@localhost:1521/prod")
     print(f"Connected to Oracle DB: Version {connection.version}")
 except Exception as err:
     print("Error while connecting to Oracle DB...", err)
