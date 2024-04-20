b3b020ef425d808f42c59985773e50d3c44097fe
Pariksheet <deparikshee2@gmail.com>
2024-04-20 18:42:10 +0530

    COMMIT

---
+++ 0.ConditionalExecution/if_else_1.py
@@ -0,0 +1,9 @@
+a = 10
+if a < 20:
+    print(f'10 is indeed lesser than 20')
+print(f'1st Program execution is done!')
+
+a = 10
+if a > 20:
+    print(f'20 is indeed greater than 10')
+print(f'2nd Program execution is done!')
