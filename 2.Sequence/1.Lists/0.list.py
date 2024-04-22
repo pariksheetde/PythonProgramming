b0172949b485b8c60213395aebd0b5a861cb3ad7
Pariksheet <deparikshee2@gmail.com>
2024-04-22 19:36:57 +0530

    COMMIT

--- 2.Sequence/1.Lists/0.list.py
+++ 2.Sequence/1.Lists/0.list.py
@@ -1,7 +1,10 @@
-# CREATE A INT LIST
+# Create a list og int type.
+# This list is homogeneous.
+# List can be homogeneous and hetrogeneous
 marks = [10, 20, 15, 25, 30, 35]
 print(marks)
 
 print(marks[0]) # returns 1st element
 print(marks[:]) # returns all the elements
 print(marks[:-1]) # returns the elements from 1st index position till 2nd last. -1 ignore the last element
+print(marks[-1]) # returns the last element.
