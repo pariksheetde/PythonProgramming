76d8f18edf14936e2ad5c7103dbc7e73df851d13
Pariksheet <deparikshee2@gmail.com>
2024-04-22 19:38:40 +0530

    COMMIT

--- 2.Sequence/1.Lists/0.list.py
+++ 2.Sequence/1.Lists/0.list.py
@@ -8,3 +8,8 @@ print(marks[0]) # returns 1st element
 print(marks[:]) # returns all the elements
 print(marks[:-1]) # returns the elements from 1st index position till 2nd last. -1 ignore the last element
 print(marks[-1]) # returns the last element.
+
+print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+marks = [10, 20, 15, 25, 30, 35]
+
