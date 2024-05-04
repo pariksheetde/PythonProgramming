553f6cbbcb240c2c2b5c015b37c6f14a696ce01a
Pariksheet <deparikshee2@gmail.com>
2024-05-04 19:31:26 +0530

    COMMIT

--- 2.Sequence/4.Strings/0.strings.py
+++ 2.Sequence/4.Strings/0.strings.py
@@ -1,26 +1,31 @@
 # STRINGS ARE HOMOGENEOUS, IMMUTABLE
+EOL = "----"
 name = 'pariksheet'
 print(f'Length : {len(name)}')
 
-print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(EOL * 37)
 
+EOL = "----"
 stmt = "What's in a name. This was stated by William Shakesphere"
 print(f'Statement : {stmt}')
 
-print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(EOL * 37)
 
+EOL = "----"
 line = 'What\'s in a name. This was stated by William Shakesphere'
 print(f'Line : {line}')
 
-print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(EOL * 37)
 
+EOL = "----"
 quote = "What\"s in a name. This was stated by William Shakesphere"
 print(f'Quote : {quote}')
 
-print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(EOL * 37)
 
+EOL = "----"
 word = "What\"s in a name. This was stated by William Shakesphere"
 print(word.split(" "))
 print(f'Type : {type(word.split(" "))}')
 
-print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+print(EOL * 37)
---
+++ 2.Sequence/4.Strings/1.strings.py
@@ -0,0 +1,15 @@
+# STRINGS ARE HOMOGENEOUS, IMMUTABLE
+EOL = "----"
+lang = 'Python'
+code = tuple(lang)
+print(f'Conversion from strings to tuple : {code}')
+
+print(EOL * 38)
+
+EOL = "----"
+alpha = 'abcdefghi'.upper()
+lst = list(alpha)
+print(f'Alphabet in List : {lst}')
+
+print(EOL * 38)
+
