6ae692865663a71fcb27d78fc2761cc2ffa34397
Pariksheet <deparikshee2@gmail.com>
2024-05-11 18:54:12 +0530

    COMMIT

--- 2.Sequence/1.Lists/3.list.py
+++ 2.Sequence/1.Lists/3.list.py
@@ -18,6 +18,16 @@ print(EOL * 37)
 
 alpha = [[0, 0, 0], [1, 1, 1], [2, 2, 2]]
 beta = marks[0:2]
+print(f'Alpha : {alpha}')
+print(f'Beta : {beta}')
 
+beta[1] = [0, 1, 0]
+print(f'After Change "Beta" : {beta}')
+
+# check to see if alpha is same as beta
+print(f'Is Alpha same as Beta : {alpha is beta}')
+print(f'Alpha\'s 1st element : {alpha[0]}')
+print(f'Beta\'s 1st element : {beta[0]}')
+print(f"Is Alpha\'s 1st {alpha[0]} element same as Beta\'s 1st {beta[0]} element? : {alpha[0] is beta[0]}")
 
 print(EOL * 37)
