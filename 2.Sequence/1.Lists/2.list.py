adc431a85747dd29ec369882436b10b0033dc8d9
Pariksheet <deparikshee2@gmail.com>
2024-05-11 12:58:58 +0530

    COMMIT

--- 2.Sequence/1.Lists/2.list.py
+++ 2.Sequence/1.Lists/2.list.py
@@ -8,6 +8,6 @@ print(EOL * 37)
 
 grades = [10, 20, 30, 40, 50]
 print(f'Top 2 grades : {grades[0:3]}')
-print(f'Last 2 grades : {grades[-1::-1][:2]}')
+print(f'Last 2 grades : {grades[-2:]}')
 
 print(EOL * 37)
--- 2.Sequence/4.Strings/3.strings.py
+++ 2.Sequence/4.Strings/3.strings.py
@@ -1,5 +1,11 @@
 EOL = "----"
 name = 'Pariksheet De'
+print(f'Return First Name : {name[0:11]}')
+print(f'Return Last Name : {name[-2:]}')
+
+print(EOL * 37)
+
+
 
 
 print(EOL * 37)
