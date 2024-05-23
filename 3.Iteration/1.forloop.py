4dcf93921583fa0d63e165b39b74b222d942402d
Pariksheet <deparikshee2@gmail.com>
2024-05-23 11:09:59 +0530

    COMMIT

--- 3.Iteration/1.forloop.py
+++ 3.Iteration/1.forloop.py
@@ -1,3 +1,15 @@
 EOL = "----"
+for i in [1, 2]:
+    i = i
+    print(f'i : {i}')
+print(EOL * 37)
 
+for i in [1, 2]:
+    i = i + i
+    print(f'i : {i}')
+print(EOL * 37)
+
+for i in range(0, 10, 2):
+    i = i + i
+    print(f'i : {i}')
 print(EOL * 37)
