2f597f8d5236bfaf296afe0e198d55fd10c63350
Pariksheet <deparikshee2@gmail.com>
2024-05-23 11:02:10 +0530

    COMMIT

---
+++ 3.Iteration/0.range.py
@@ -0,0 +1,21 @@
+EOL = "----"
+# Converting RANGE into list and tuple
+ask = range(10)
+print(list(ask))
+print(tuple(ask))
+
+print(EOL * 37)
+
+# Converting RANGE into list and tuple
+bid = range(1,10,2)
+print(list(bid))
+print(tuple(bid))
+
+print(EOL * 37)
+
+# Converting RANGE into list and tuple
+token = range(1,10,3)
+print(list(token))
+print(tuple(token))
+
+print(EOL * 37)
