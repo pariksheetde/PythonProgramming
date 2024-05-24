f918462a0d4d4f2ea5d4e0053747aaa3bd8553f4
Pariksheet <deparikshee2@gmail.com>
2024-05-24 11:39:54 +0530

    COMMIT

---
+++ TimBuchalka/1.Sequences/1.Strings/0.strings.py
@@ -0,0 +1,11 @@
+print('Today is good day to learn Python')
+print('Python\'s single quote is easy to learn')
+print('We can also include "double quotes" in strings')
+print()
+
+print("Hellow " + "World")
+print()
+
+greeting = 'Hi!'
+name = 'Pariksheet De'
+print(f'{greeting} {name}')
