600673cc74694fab76194fd2432af3912787cb49
Pariksheet <deparikshee2@gmail.com>
2024-05-24 12:06:49 +0530

    COMMIT

--- TimBuchalka/1.Sequences/1.Strings/0.strings.py
+++ TimBuchalka/1.Sequences/1.Strings/0.strings.py
@@ -9,3 +9,19 @@ print()
 greeting = 'Hi!'
 name = 'Pariksheet De'
 print(f'{greeting} {name}')
+
+print('The pet owner said "No no, \'e\'s uh.., he\'s resting"')
+print("The pet owner said 'No no, \"e\"s uh.., he\"s sleepng'")
+print("""The pet owner said 'No no, \"e\"s uh.., he\"s sleepng'""")
+print('''The pet owner said "No no, \'e\'s uh.., he\'s resting"''')
+print("""This string
+has been
+split over
+multiple lines
+""")
+print("""This string \
+has been \
+split over \
+multiple lines \
+""")
+print()
