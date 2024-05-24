b50a12bd46c90094613e7d5cf9cb1e4408b148ba
Pariksheet <deparikshee2@gmail.com>
2024-05-24 13:16:51 +0530

    COMMIT

--- TimBuchalka/1.Sequences/1.Strings/2.strings.py
+++ TimBuchalka/1.Sequences/1.Strings/2.strings.py
@@ -3,13 +3,13 @@ EOL = "----"
 stmt = "What\"s in a name - William Shakespere"
 print(stmt)
 print()
-print(EOL * 37)
+print()
 
 print('Let\'s try this with separator/sep', 'Ok',sep='\n')
 print()
-print(EOL * 37)
+print()
 
 # SEP has been used to break line
 print('What\"s in a name - William Shakespere', 'For every action there is an equal or opposite reactions', sep='\n')
 print()
-print(EOL * 37)
+print()
