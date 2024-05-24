69d52b5ee4cd008bb9a28e1a5c661d9b2594c867
Pariksheet <deparikshee2@gmail.com>
2024-05-24 10:49:53 +0530

    COMMIT - Tim - 0.Getting with with Basics

--- TimBuchalka/0.Getting Started with Basics/helloworld.py
+++ TimBuchalka/0.Getting Started with Basics/helloworld.py
@@ -1,3 +1,15 @@
+EOL = "----"
+
 stmt = "What\"s in a name - William Shakespere"
 print(stmt)
+print()
+print(EOL * 37)
 
+print('Let\'s try this with separator/sep', 'Ok',sep='\n')
+print()
+print(EOL * 37)
+
+# SEP has been used to break line
+print('What\"s in a name - William Shakespere', 'For every action there is an equal or opposite reactions', sep='\n')
+print()
+print(EOL * 37)
