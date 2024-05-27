13087f88e58afef66888468ad6a851379dee8419
Pariksheet <deparikshee2@gmail.com>
2024-05-27 18:59:09 +0530

    COMMIT

---
+++ TimBuchalka/1.Sequences/1.Strings/3.strings.py
@@ -0,0 +1,14 @@
+a = 12
+b = 3
+
+print(a + b)        # 15
+print(a - b)        # 9
+print(a * b)        # 36
+print(a / b)        # 4.0
+print(a // b)       # 4
+print(a % b)        # 0
+
+print()
+
+for indx in range(0, a // b):
+    print(indx)
