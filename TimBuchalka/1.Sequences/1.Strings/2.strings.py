a40aab2698e9eaf55ca9dd5add4e10d70c60d90d
Pariksheet <deparikshee2@gmail.com>
2024-05-27 19:41:04 +0530

    COMMIT

--- TimBuchalka/1.Sequences/1.Strings/2.strings.py
+++ TimBuchalka/1.Sequences/1.Strings/2.strings.py
@@ -10,3 +10,6 @@ print()
 # SEP has been used to break line
 print('What\"s in a name - William Shakespere', 'For every action there is an equal or opposite reactions', sep='\n')
 print()
+
+parrot = 'Norwegian Blue'
+print(parrot[3:5])
