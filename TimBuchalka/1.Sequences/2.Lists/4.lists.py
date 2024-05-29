5b5285d514593da87292f7b14cdc0bf016c9d770
Pariksheet <deparikshee2@gmail.com>
2024-05-29 13:18:34 +0530

    COMMIT

---
+++ TimBuchalka/1.Sequences/2.Lists/4.lists.py
@@ -0,0 +1,28 @@
+# SORTED METHOD
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+numbers = sorted(even + odd)
+print("Concatenating {} & {} : {}".format(even, odd, numbers))
+print()
+
+# EXTEND & SORT METHOD
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+odd.extend(even)
+odd.sort()
+print("Using extend() method :{}".format(odd))
+print()
+
+# SORTED METHOD
+digits = sorted("432985617")
+print("Digits : {}".format(digits))
+print()
+
+# COPY METHOD
+age = [2, 4, 6, 8, 10, 12]
+copy = age.copy()
+print("Age : {}".format(age))
+print("Copy : {}".format(copy))
+print(age is copy)
+print(age == copy)
+print()
