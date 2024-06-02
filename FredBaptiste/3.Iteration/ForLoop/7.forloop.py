d2a01991da7b04e2087bea00286852cec16b4846
Pariksheet <deparikshee2@gmail.com>
2024-06-02 13:06:53 +0530

    COMMIT

---
+++ FredBaptiste/3.Iteration/ForLoop/7.forloop.py
@@ -0,0 +1,21 @@
+# SEQUENCE IS A ORDERED COLLECTION OF ELEMENTS
+# IF THE SEQUENCE ISN'T ORDERED, WE COULDN'T REFER THESE ELEMENTS BY THEIR INDEX POSITION
+# STRING, LIST and TUPLES ARE SEQUENCE
+# STRINGS ARE IMMUTABLE THAT MEANS THEY CANN'T BE CHANGES
+# LISTS ARE MUTABLE, THAT MEANS THE VALUE CAN BE CHANGED
+
+
+shopping_cart = [
+    'milk',
+    'pasta',
+    'spam',
+    'bread',
+    'rice'
+]
+
+shopping_cart += ['butter']
+fav_shopping_cart = shopping_cart
+fav_shopping_cart.append('cherry')
+print(f'Shopping Cart has: {shopping_cart}')
+print(f'Favourite Shopping Cart has: {fav_shopping_cart}')
+print()
