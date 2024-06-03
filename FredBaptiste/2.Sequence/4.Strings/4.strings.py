462b5759b3bad4a9ed634df5dd3bc5ed3aef404b
Pariksheet <deparikshee2@gmail.com>
2024-06-03 12:38:14 +0530

    COMMIT

---
+++ FredBaptiste/2.Sequence/4.Strings/4.strings.py
@@ -0,0 +1,12 @@
+pangram = "The quick brown fox jumps over the lazy fox"
+print(sorted(pangram))
+print()
+
+# SORT() & SORTED()
+numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
+numbers.sort()
+sorted_numbers = sorted(numbers)
+print(f'Using Sorted : {sorted(sorted_numbers)}')
+print(f'Using Sort : {sorted(numbers)}')
+print(f'Using Sort : {numbers}')
+print()
