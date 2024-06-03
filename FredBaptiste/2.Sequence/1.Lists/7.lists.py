9201f76ea26a2b86463a91e3ff88a32f61b73210
Pariksheet <deparikshee2@gmail.com>
2024-06-03 13:49:50 +0530

    COMMIT

---
+++ FredBaptiste/2.Sequence/1.Lists/7.lists.py
@@ -0,0 +1,22 @@
+fruits = [
+    'apple',
+    'watermelon',
+    'guava',
+    'blackberry',
+]
+print(fruits[-1:-3:-1])
+print(f'Before change: {fruits}')
+fruits[-1:-3:-1] = ['banana', 'grapes']
+print(f'After change: {fruits}')
+print()
+
+fruits = [
+    'apple',
+    'strawberry',
+    'grapes'
+    'watermelon',
+    'banana',
+    'blackberry',
+]
+fruits[0:2] = ['pears', 'dragon fruit']
+print(f'After Change: {fruits}')
