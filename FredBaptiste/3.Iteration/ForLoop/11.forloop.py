67f5222757183a804b1959480c886abda676c883
Pariksheet <deparikshee2@gmail.com>
2024-06-03 10:46:01 +0530

    COMMIT

---
+++ FredBaptiste/3.Iteration/ForLoop/11.forloop.py
@@ -0,0 +1,38 @@
+# SEQUENCE IS A ORDERED COLLECTION OF ELEMENTS
+# IF THE SEQUENCE ISN'T ORDERED, WE COULDN'T REFER THESE ELEMENTS BY THEIR INDEX POSITION
+# STRING, LIST and TUPLES ARE SEQUENCE
+# STRINGS ARE IMMUTABLE THAT MEANS THEY CANN'T BE CHANGES
+# LISTS ARE MUTABLE, THAT MEANS THE VALUE CAN BE CHANGED
+
+# WRITE A CODE TO GENERATE 2 LISTS
+# "FLOWER" WILL GO TO NEW LIST AND "SHRUB" WILL GO TO NEW LIST
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+flower_pot = []
+shrub_pot = []
+for flower in data:
+    if 'Flower' in flower:
+        flower_pot.append(flower)
+    elif 'Shrub' in flower:
+        shrub_pot.append(flower)
+print(f'Flower Pot contains {flower_pot}')
+print(f'Shrub Pot contains {shrub_pot}')
