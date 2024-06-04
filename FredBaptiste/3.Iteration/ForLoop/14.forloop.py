a8039c7af5e37318fbd801361c30c93ef5aab5b8
Pariksheet <deparikshee2@gmail.com>
2024-06-04 18:44:25 +0530

    COMMIT

--- FredBaptiste/3.Iteration/ForLoop/14.forloop.py
+++ FredBaptiste/3.Iteration/ForLoop/14.forloop.py
@@ -1,13 +1,13 @@
-# print the elements from forward staring with 0
-marks = [10, 15, 20, 25, 30, 35, 40]
-print("print the elements from forward staring with 0")
-for index, value in enumerate(marks):
-    print(index, value)
-print()
+# SEQUENCE IS A ORDERED COLLECTION OF ELEMENTS
+# IF THE SEQUENCE ISN'T ORDERED, WE COULDN'T REFER THESE ELEMENTS BY THEIR INDEX POSITION
+# STRING, LIST and TUPLES ARE SEQUENCE
+# STRINGS ARE IMMUTABLE THAT MEANS THEY CANN'T BE CHANGES
+# LISTS ARE MUTABLE, THAT MEANS THE VALUE CAN BE CHANGED
 
-# print the elements from backward staring with highest available index value
-marks = [10, 15, 20, 25, 30, 35, 40]
-print("print the elements from backward staring with highest available index value")
-for index in range(len(marks) - 1, -1, -1):
-    print(index)
-print()
+# DELETE THE ELEMENTS THAT ARE GREATER THAN 200 OR LESSER THAN 100
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150, 
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+min_valid = 100
+max_valid = 200
+# processing high values in the list
