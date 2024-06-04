a570e69fb2c1ff508ca1797d27053841d6a37b6e
Pariksheet <deparikshee2@gmail.com>
2024-06-04 14:27:45 +0530

    COMMIT

---
+++ FredBaptiste/3.Iteration/ForLoop/13.forloop.py
@@ -0,0 +1,21 @@
+# SEQUENCE IS A ORDERED COLLECTION OF ELEMENTS
+# IF THE SEQUENCE ISN'T ORDERED, WE COULDN'T REFER THESE ELEMENTS BY THEIR INDEX POSITION
+# STRING, LIST and TUPLES ARE SEQUENCE
+# STRINGS ARE IMMUTABLE THAT MEANS THEY CANN'T BE CHANGES
+# LISTS ARE MUTABLE, THAT MEANS THE VALUE CAN BE CHANGED
+
+# DELETE THE ELEMENTS THAT ARE LESSER THAN 100 OR GREATER THAN 200
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150, 
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+min_valid = 100
+max_valid = 200
+# processing high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_valid:
+        start = index
+        break
+print(start)
+del data[start:]
+print(data)
