956f8a63299553b28f231a20bfd400bceafa103c
Pariksheet <deparikshee2@gmail.com>
2024-06-04 19:09:54 +0530

    COMMIT

--- FredBaptiste/3.Iteration/ForLoop/14.forloop.py
+++ FredBaptiste/3.Iteration/ForLoop/14.forloop.py
@@ -5,9 +5,14 @@
 # LISTS ARE MUTABLE, THAT MEANS THE VALUE CAN BE CHANGED
 
 # DELETE THE ELEMENTS THAT ARE GREATER THAN 200 OR LESSER THAN 100
-data = [4, 5, 104, 105, 110, 120, 130, 130, 150, 
-        160, 170, 183, 185, 187, 188, 191, 350, 360]
+data = [104, 101, 4, 105, 308, 103, 5,
+        107, 100, 306, 106, 102, 108]
 
 min_valid = 100
 max_valid = 200
 # processing high values in the list
+for index in range(len(data) - 1, -1, -1):
+    if data[index] < min_valid or data[index] > max_valid:
+        del data[index]
+print(data)
+print()
