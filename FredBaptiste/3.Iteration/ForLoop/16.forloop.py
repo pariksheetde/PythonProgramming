32a026bf7d351b1418ae5a0c4a373f69d9435f34
Pariksheet <deparikshee2@gmail.com>
2024-06-05 09:59:44 +0530

    COMMIT

--- FredBaptiste/3.Iteration/ForLoop/16.forloop.py
+++ FredBaptiste/3.Iteration/ForLoop/16.forloop.py
@@ -10,9 +10,12 @@ data = [1, 7, 104, 101, 4, 105, 308, 103, 5,
 
 min_valid = 100
 max_valid = 200
-# processing high values in the list
-for index in range(len(data) - 1, -1, -1):
-    if data[index] < min_valid or data[index] > max_valid:
-        del data[index]
-print(data)
+
+top_index = len(data) - 1
+for index, value in enumerate(reversed(data)):
+    if value < min_valid or value > max_valid:
+        del data[top_index - index]
+        print(top_index - index, value)
+print(f'Data after deletion: {data}')
 print()
+
