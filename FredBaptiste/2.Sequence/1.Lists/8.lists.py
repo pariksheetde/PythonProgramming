33416a0ad66761bbbb1deee6ac7447dc509ceced
Pariksheet <deparikshee2@gmail.com>
2024-06-05 14:19:57 +0530

    COMMIT

---
+++ FredBaptiste/2.Sequence/1.Lists/8.lists.py
@@ -0,0 +1,17 @@
+flowers = [
+    "Daffodil",
+    "Sunflower",
+    "Lavender",
+    "Tiger Lily",
+    "Evening Primrose",
+    "Hydrangea"
+]
+
+# Solution 1
+for flower in flowers:
+    print(flower)
+print()
+
+# Solution 2
+print(" | ".join(flowers))
+print()
