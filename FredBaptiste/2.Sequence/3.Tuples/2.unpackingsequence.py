fb1fa8192e87c5aa85855369d47175be314d0076
Pariksheet <deparikshee2@gmail.com>
2024-06-06 20:18:47 +0530

    COMMIT

--- FredBaptiste/2.Sequence/3.Tuples/2.unpackingsequence.py
+++ FredBaptiste/2.Sequence/3.Tuples/2.unpackingsequence.py
@@ -18,3 +18,8 @@ print(f'inflation : {inflation}')
 for index in enumerate(range(10,21)):
     id, value = index
     print('ID: {} - Value: {}'.format(id, value))
+print()
+
+coffee_table = ("Cappuccino", 100, 75, 34.5)
+print('{} was ordered in Table No: {}. Price is {}. Cost of bill was {}'.format(coffee_table[0], coffee_table[1], coffee_table[3], coffee_table[3] * 1.165),'$')
+print(f'{coffee_table[0]} was ordered in Table No: {coffee_table[1]}. Price is {coffee_table[3]}. Cost of bill was $ {coffee_table[3] * 1.165}')
