e1f41051881313a9aa47db689d0f6ea67d9a316e
Pariksheet <deparikshee2@gmail.com>
2024-06-08 21:40:27 +0530

    COMMIT

--- FredBaptiste/2.Sequence/3.Tuples/2.unpackingsequence.py
+++ FredBaptiste/2.Sequence/3.Tuples/2.unpackingsequence.py
@@ -2,3 +2,5 @@ details = ('Monica', 'Bellucci', 'Italy', 'Female', 'Model')
 print(f'Name: {details[0],details[1]}')
 print(f'Profession: {details[4]}')
 print("{} belongs from {} and her profession is {}".format(details[0],details[1],details[4]))
+print()
+
