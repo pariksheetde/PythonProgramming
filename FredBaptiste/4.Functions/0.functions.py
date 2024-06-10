e3add785ce67d9d65d4d75d672f33271d12357e6
Pariksheet <deparikshee2@gmail.com>
2024-06-10 20:04:43 +0530

    COMMIT

--- FredBaptiste/4.Functions/0.functions.py
+++ FredBaptiste/4.Functions/0.functions.py
@@ -1,8 +1,11 @@
-def add(x, y):
+def add(x, y = 5):
+    """This Function Is Used To Perform Addition Of 2 Numbers.
+    2Nd Parameter Has A Default Value Of 5
+    """
     result = (x * y)
     return result
 
-print(f'The result of summation: {add(7,5)}')
+print(f'The result of summation: {add(y = 7,x = 10)}')
 print()
 
 for val in range(1,5):
