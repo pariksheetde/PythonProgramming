ec6471ea2a557222e1da6016aec3f5836b32a511
Pariksheet <deparikshee2@gmail.com>
2024-06-10 18:26:11 +0530

    COMMIT

--- FredBaptiste/4.Functions/1.functions.py
+++ FredBaptiste/4.Functions/1.functions.py
@@ -1,10 +1,26 @@
 # WRITE A PALIDROME FUNCTION TO CHECK IF THE REVERSE STRING WORKS
+# SOME OF THE EXAMPLES OF PALIDROMES ARE (RACECAR, MADAM)
 def is_palidrome(string):
     if str.casefold(string) == str.casefold(string[::-1]):
-        print("{} is a palidrome".format(string))
+        print("\'{}\' is a palidrome".format(string))
     else:
-        print("{} is not a palidrome".format(string))
+        print("\'{}\' is not a palidrome".format(string))
 
 prompt = input("Enter a word to check if the word is palidrome: ")
 is_palidrome(prompt)
 print()
+
+def is_sentence_palidrome(string):
+    string = ""
+    for char in string:
+        if char.isalnum():
+            string += char
+    print(string)
+    return str.casefold(string) == str.casefold(string[::-1])
+
+
+word = input("Enter a sentence to check if the word is palidrome: ")
+if is_sentence_palidrome(word):
+    print('{} is a palidrome'.format(word))
+else:
+    print('{} is not a palidrome'.format(word))    
