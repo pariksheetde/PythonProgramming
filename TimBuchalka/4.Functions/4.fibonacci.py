3bf13406342481499d23d025ee7392e91623bdc5
Pariksheet <deparikshee2@gmail.com>
2024-06-11 10:13:15 +0530

    COMMIT

---
+++ TimBuchalka/4.Functions/4.fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    """
+    Return the `n`th Fibonacci number, for positive `n`th number
+    """
+    if 1 <= n <= 1:
+        return n
+    a, b = 1, 0
+    result = None
+    for f in range(n - 1):
+        result = a + b
+        b = a
+        a = result
+    return result
+
+
+for i in range(7):
+    total = fibonacci(i)
+    print(total)
