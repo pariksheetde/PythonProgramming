6a3852576a1b9dc4ea1b00a3fac458ee6b12ef1d
Pariksheet <deparikshee2@gmail.com>
2024-06-15 18:29:55 +0530

    COMMIT

--- FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
+++ FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
@@ -12,3 +12,17 @@ b = 0
 result = a / b if b != 0 else None
 print(f'Result of Ternary Operator: {result}')
 print()
+
+ask_price = 100
+
+volume = 50 if ask_price > 50 else 80
+print(f'Result of Ternary Operator: {volume}')
+print()
+
+current_value = 100
+running_total = 15_000
+running_count = 125
+
+clean_value = -999 if current_value == -999 else current_value
+print(f'Clean Value: {clean_value + current_value}')
+print()
