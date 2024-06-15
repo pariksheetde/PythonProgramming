e237df34bcf801eaec9c7b279c7920c403574513
Pariksheet <deparikshee2@gmail.com>
2024-06-15 18:03:31 +0530

    COMMIT

--- FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
+++ FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
@@ -5,3 +5,9 @@ bonus = ((CTC * 30) / 100) if rating == 1 else ((CTC * 10) / 100)
 hike = ((CTC * 8) / 100) if rating == 1 else ((CTC * 10) / 100)
 print(f'Next CTC: {bonus + CTC + hike}')
 print()
+
+a = 10
+b = 6
+
+result = a / b if b != 0 else None
+print(f'Result of Ternary Operator: {result}')
