4ed4282d7f00af0ef1e3a697ac22d327a2329e86
Pariksheet <deparikshee2@gmail.com>
2024-06-15 17:58:59 +0530

    COMMIT

--- FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
+++ FredBaptiste/1.ConditionalExpression/3.ternary_operator.py
@@ -1,7 +1,7 @@
-rating = 1
+rating = 2
 CTC = 35_00_000
 
-bonus = ((CTC * 30) / 100) if rating else "Default"
-hike = ((CTC * 8) / 100) if rating else "Default"
+bonus = ((CTC * 30) / 100) if rating == 1 else ((CTC * 10) / 100)
+hike = ((CTC * 8) / 100) if rating == 1 else ((CTC * 10) / 100)
 print(f'Next CTC: {bonus + CTC + hike}')
 print()
