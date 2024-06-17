2d8064393be8fdde479ea9bdc19af2ac8ccc9d2f
Pariksheet <deparikshee2@gmail.com>
2024-06-17 15:12:53 +0530

    COMMIT

--- FredBaptiste/2.Sequence/4.Strings/6.strings.py
+++ FredBaptiste/2.Sequence/4.Strings/6.strings.py
@@ -11,3 +11,20 @@ sentence = "To be or not to be"
 print(f'1st Character: {sentence[0]}')
 print(f'Last Character: {sentence[len(sentence) - 1]}')
 print()
+
+lst = [1, 2, 3]
+print(lst * 3)
+ask = "+/+/"
+print(ask * 2)
+print()
+l = [0] * 10
+print(l)
+print()
+
+matrix = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+print(matrix[0][0], matrix[1][1], matrix[2][2])
+print()
