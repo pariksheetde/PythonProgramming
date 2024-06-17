49544a40e956f00a250d191569177606e37440ac
Pariksheet <deparikshee2@gmail.com>
2024-06-17 14:52:10 +0530

    COMMIT

--- FredBaptiste/2.Sequence/4.Strings/6.strings.py
+++ FredBaptiste/2.Sequence/4.Strings/6.strings.py
@@ -5,3 +5,9 @@ print()
 stmt = 'Python\'s best feature'
 print(stmt)
 print()
+
+# print 1st and last characters from the string
+sentence = "To be or not to be"
+print(f'1st Character: {sentence[0]}')
+print(f'Last Character: {sentence[len(sentence) - 1]}')
+print()
