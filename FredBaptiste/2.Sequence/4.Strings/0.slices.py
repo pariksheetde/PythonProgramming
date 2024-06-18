6aca387f1803a24c70ce257ef152f4ce322a058b
Pariksheet <deparikshee2@gmail.com>
2024-06-18 12:35:49 +0530

    COMMIT

---
+++ FredBaptiste/2.Sequence/4.Strings/0.slices.py
@@ -0,0 +1,14 @@
+# display only the 2nd occurance of the element
+marks = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+print(marks[2::2])
+print()
+
+# display only the 2nd occurance of the element from backward
+marks = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+print(marks[::-2])
+print()
+
+# display only the 3rd occurance of the element from backward
+marks = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
+print(marks[::-3])
+print()
