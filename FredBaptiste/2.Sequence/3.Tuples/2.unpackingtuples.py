c4c01372b521d1d39b234cdb509a2967d0e466e7
Pariksheet <deparikshee2@gmail.com>
2024-06-18 20:53:35 +0530

    COMMIT

--- FredBaptiste/2.Sequence/3.Tuples/2.unpackingtuples.py
+++ FredBaptiste/2.Sequence/3.Tuples/2.unpackingtuples.py
@@ -12,12 +12,20 @@ print(f'APR: {apr}')
 print(f'APY: {apy}')
 print()
 
-# swapping 2 variables
+# swapping 2 variables using temporary variable
 a = 100
 b = 200
 temp = a
 a = b
 b = temp
+print(f'After Swapping, \'A\': {a}')
+print(f'After Swapping, \'B\': {b}')
+print()
+
+# swapping 2 variables using unpacking
+a = 100
+b = 200
+tmp = b, a
 print(f'After Swapping, \'A\': {a}')
 print(f'After Swapping, \'B\': {b}')
 print()
