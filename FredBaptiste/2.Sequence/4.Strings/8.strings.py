218c90bd2c938789494c2697941719bbd6bce490
Pariksheet <deparikshee2@gmail.com>
2024-06-18 11:42:02 +0530

    COMMIT

--- FredBaptiste/2.Sequence/4.Strings/8.strings.py
+++ FredBaptiste/2.Sequence/4.Strings/8.strings.py
@@ -30,3 +30,28 @@ if level_0 is matrix_reloaded[0]:
     print(f"Both the objects references are same")
 else:
     print(f"Objects references are not same")
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# convert the string into tuple
+tup = tuple(alphabet)
+print(tup)
+# convert the string into list
+lst = list(alphabet)
+print(lst)
+print()
+
+alpha = ([10, 15], 30)
+beta = alpha * 3
+print(beta)
+
+if alpha[0] is beta[0]:
+    print(f"alpha[0] is beta[0] objects references are same")
+
+if alpha[0] is beta[2]:
+    print(f"alpha[0] is beta[2] objects references are same")
+
+if alpha[0] is beta[4]:
+    print(f"alpha[0] is beta[4] objects references are same")
+else:
+    print(f"Objects references are not same")
+print()
