31e898f534fb5698764a8344f10a291564cadf25
Pariksheet <deparikshee2@gmail.com>
2024-06-18 12:51:34 +0530

    COMMIT

--- FredBaptiste/2.Sequence/4.Strings/0.slices.py
+++ FredBaptiste/2.Sequence/4.Strings/0.slices.py
@@ -12,3 +12,13 @@ print()
 marks = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90]
 print(marks[::-3])
 print()
+
+# display last 2 characters
+name = "Pariksheet De"
+surname = name[-2:]
+last_name = name[11:]
+reversed_name = name[::-1]
+print(surname)
+print(last_name)
+print(reversed_name)
+print()
