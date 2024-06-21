a5dd77fa2cadaa9590e844a9d418e01fb8159f12
Pariksheet <deparikshee2@gmail.com>
2024-06-21 21:31:00 +0530

    COMMIT

--- TimBuchalka/0.Basics/0.basics.py
+++ TimBuchalka/0.Basics/0.basics.py
@@ -7,3 +7,37 @@ print(stmt)
 print("Let's print in this line in double quotes")
 print('Let"s print in this line in single quotes')
 print()
+
+greeting = "Hello"
+name = "Pariksheet"
+print(greeting + ' ' + name)
+print()
+
+splitstring = "This sentence will have some special characters.\nLet's see & learn.\tIntersting huh!.\nThis line is \
+is going to be split in multiple lines"
+print(splitstring)
+print()
+
+tabbedstring = '1\t2\t3\t4\t5'
+print(f'Tabbed Numbers: {tabbedstring}')
+print()
+
+print('The pet owner said "No, no, \'e\'s uh..he\'s sleeping"')
+print("The pet owner said 'No, no, \'e\'s uh..he\'s sleeping'")
+print("""
+The pet owner said 'No, no, \'e\'s uh..he\'s sleeping'
+""")
+print()
+
+spliitedlines = """This line is splitted 
+over several line.
+Let's see, how it's read"""
+print(spliitedlines)
+print()
+
+spliitedquotes = """This line is written \
+over several line. \
+But it's written in a single line. \
+Let's see, how it's read \
+"""
+print(spliitedquotes)
