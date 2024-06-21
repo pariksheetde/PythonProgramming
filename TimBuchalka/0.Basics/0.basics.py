6c3ccd015890df33527f172374a57451dea554c1
Pariksheet <deparikshee2@gmail.com>
2024-06-21 21:01:48 +0530

    COMMIT

---
+++ TimBuchalka/0.Basics/0.basics.py
@@ -0,0 +1,9 @@
+print("Hello World!", 'Let\s start learning Python Programming', sep="\n")
+print()
+
+stmt = "To be or not to be"
+print("Today is good day to start learning Python prograramming")
+print(stmt)
+print("Let's print in this line in double quotes")
+print('Let"s print in this line in single quotes')
+print()
--- TimBuchalka/Basics/0.basics.py
+++
@@ -1,2 +0,0 @@
-print("Hello World!", 'Let\s start learning Python Programming', sep="\n")
-print()
