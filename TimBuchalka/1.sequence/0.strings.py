f7dacf328f22412545b0d6fb8370da2876d0baa8
Pariksheet <deparikshee2@gmail.com>
2024-06-22 16:55:38 +0530

    COMMIT

---
+++ TimBuchalka/1.sequence/0.strings.py
@@ -0,0 +1,20 @@
+parrot = "Norwegian Blue"
+print(f'Length of parrot: {len(parrot)}')
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+
