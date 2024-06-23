503abb804eccfc945efca4d78cdf50d454593ba5
Pariksheet <deparikshee2@gmail.com>
2024-06-23 19:37:40 +0530

    COMMIT

--- TimBuchalka/0.Basics/2.basics.py
+++ TimBuchalka/0.Basics/2.basics.py
@@ -63,5 +63,19 @@ print("There are {0} days in Jan, Mar, May, Jul, Aug, Oct, Dec"
       .format(31))
 print("Jan: {0}, Feb: {1}, Mar: {0}, Apr: {2}, May: {0}, Jun: {2}, Jul: {0}, Aug: {0}, Sep: {2}, Oct: {0}, Nov: {2}, Dec: {0}"
       .format(31, 28, 30))
+print("""
+      Non Leap Year Calender
+      Jan: {0}
+      Feb: {1}
+      Mar: {0}
+      Apr: {2}
+      May: {0}
+      Jun: {2}
+      Jul: {0}
+      Aug: {0}
+      Sep: {2}
+      Oct: {0}
+      Nov: {2}
+      Dec: {0}""".format(31, 28, 30))
 
 print()
