23c04a71380bf3b6409caa0ea8bc82debe2ceecc
Pariksheet <deparikshee2@gmail.com>
2024-06-24 12:42:31 +0530

    COMMIT

--- TimBuchalka/1.sequence/2.lists/6.lists.py
+++ TimBuchalka/1.sequence/2.lists/6.lists.py
@@ -1,30 +1,25 @@
-available_parts = [
-    'Computer', 
-    'Monitor', 
-    'Keyboard', 
-    'Mouse', 
-    'Mouse Mat', 
-    'DVD Drive',
-    'HDMI Cable']
-# valid_choice = [str(i) for i in range(1, len(available_parts) + 1)]
-another_valid_choice = []
-for indx in range(1, len(available_parts) + 1):
-    another_valid_choice.append(str(indx))
-print(another_valid_choice)
-current_choice = '-'
-computer_parts = []
-
-while current_choice != '0':
-    if current_choice in another_valid_choice:
-        print(f'Adding {current_choice}')
-        index = int(current_choice) - 1
-        chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
-    else:
-        print("Add option from below list")
-        for index, part in enumerate(available_parts):
-            print(f'{index + 1}: {part}')
-    current_choice = input()
-    print(f'{computer_parts} added to your cart')
+flowers_shrubs = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Flower",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Liliac - Shrub",
+    "Mangolia - Shrub"
+]
+flower = []
+shrub = []
+for garden in flowers_shrubs:
+    if 'Shrub' in garden:
+        shrub.append(garden)
+    elif 'Flower' in garden:
+        flower.append(garden)
+print(f'Shrub: {shrub}')
+print(f'Flower: {flower}')
 
 print()
