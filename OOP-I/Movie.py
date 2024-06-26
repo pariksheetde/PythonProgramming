0bcabf81cc5c525271adb7f68cf783aab8dfe2bd
Pariksheet <deparikshee2@gmail.com>
2024-06-26 14:43:39 +0530

    COMMIT

--- OOP-I/Movie.py
+++ OOP-I/Movie.py
@@ -1,14 +1,14 @@
 class Movie:
-    def __init__(self, title, year, language, rating) -> None:
+    def __init__(self, title, year, language):
         self.title = title
         self.year = year
         self.language = language
-        self.rating = rating
+        self.rating = 9
 
-titanic = Movie("Titanic", 1997, "English", 8.7)
+titanic = Movie("Titanic", 1997, "English")
 print(f'{titanic.title} was released in the year {titanic.year} in {titanic.language} and rating was {titanic.rating}')
 
-jurassic_park = Movie("Jurassic Park", 1993, "English", 8.7)
+jurassic_park = Movie("Jurassic Park", 1993, "English")
 print(f'{jurassic_park.title} was released in the year {jurassic_park.year} in {jurassic_park.language} and rating was {jurassic_park.rating}')
 
 print()
