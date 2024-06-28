2c94045cc90ccda35a7884517df38ba3e80183bf
Pariksheet <deparikshee2@gmail.com>
2024-06-28 10:44:11 +0530

    COMMIT

--- OOP-I/ClassAttributes/Dog.py
+++ OOP-I/ClassAttributes/Dog.py
@@ -3,10 +3,13 @@ class Dog:
     def __init__(self, name, age, breed):
         self.name = name
         self.age = age
-        self.breed = breed
+        self._breed = breed    # _breeed is non-public attribute. non-public attribute shouldn't be accessed outside the class
 
 kite = Dog('Kite', 1.2, 'English')
 print(f'Let\'s see the species: {Dog.species}')
-print(f'I have {Dog.species} which is {kite.breed} breed. His name is {kite.name} and age is {kite.age}')
+print(f'I have {Dog.species} which is {kite._breed} breed. His name is {kite.name} and age is {kite.age}')
+
+kite._breed = 'American'
+print(f'After changing the non-public attribute: {kite._breed}')
 
 print()
--- OOP-I/ClassAttributes/Movies.py
+++ OOP-I/ClassAttributes/Movies.py
@@ -2,16 +2,16 @@ class Movie:
     id_counter = 0
     def __init__(self, title, year):
         self.id = Movie.id_counter
-        self.title = title
+        self._title = title     # _title is non-public attribute. non-public attribute shouldn't be accessed outside the class
         self.year = year
 
         Movie.id_counter += 1
 
 venom = Movie("Venom", 2018)
-print(f'ID: {Movie.id_counter}. {venom.title} was released in the year {venom.year}')
+print(f'ID: {Movie.id_counter}. {venom._title} was released in the year {venom.year}')
 
 mib = Movie("MIB", 2008)
-print(f'ID: {Movie.id_counter}. {mib.title} was released in the year {mib.year}')
+print(f'ID: {Movie.id_counter}. {mib._title} was released in the year {mib.year}')
 
 
 print()
