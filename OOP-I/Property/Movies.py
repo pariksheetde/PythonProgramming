d5c924e8cd76af29cb0862e7c90868e7245ffbcd
Pariksheet <deparikshee2@gmail.com>
2024-06-28 16:50:51 +0530

    COMMIT

--- OOP-I/Property/Movies.py
+++ OOP-I/Property/Movies.py
@@ -25,7 +25,7 @@ print(f'I loved "{titanic.title}". Rating was {titanic.get_rating()}. But someho
 
 print("Titanic movie rating was not correct, it's per below the actual so need to update the rating \
 Let's update the rating")
-titanic.set_rating(9.5)
+titanic.set_rating(1)
 print(f'New rating for {titanic.title}: {titanic.get_rating()}')
 
 print()
