a1d7a642d4ff4455ee6a166ad769abaf9baf5fac
Pariksheet <deparikshee2@gmail.com>
2024-06-28 11:13:53 +0530

    COMMIT

--- OOP-I/ClassAttributes/Movies.py
+++ OOP-I/ClassAttributes/Movies.py
@@ -2,16 +2,16 @@ class Movie:
     id_counter = 0
     def __init__(self, title, year):
         self.id = Movie.id_counter
-        self._title = title     # _title is non-public attribute. non-public attribute shouldn't be accessed outside the class
-        self.year = year
+        self._title = title   ## _title is non-public attribute. non-public attribute shouldn't be accessed outside the class. It's meant to be internally used by the class.
+        self.__year = year    ## __year is name mangling. non-public attribute shouldn't be accessed outside the class. It's meant to be internally used by the class.
 
         Movie.id_counter += 1
 
 venom = Movie("Venom", 2018)
-print(f'ID: {Movie.id_counter}. {venom._title} was released in the year {venom.year}')
+print(f'ID: {Movie.id_counter}. {venom._title} was released in the year {venom._Movie__year}')
 
 mib = Movie("MIB", 2008)
-print(f'ID: {Movie.id_counter}. {mib._title} was released in the year {mib.year}')
+print(f'ID: {Movie.id_counter}. {mib._title} was released in the year {mib._Movie__year}')
 
 
 print()
