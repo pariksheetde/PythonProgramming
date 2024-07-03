b545cb23883d4424cc71f7cc5087fbc4ff2477ed
Pariksheet <deparikshee2@gmail.com>
2024-07-03 18:37:14 +0530

    COMMIT

--- OOP-I/Methods/Circle.py
+++ OOP-I/Methods/Circle.py
@@ -1,13 +1,22 @@
 class Circle:
 
-    def __init__(self, radius):
+    PI = 3.1416
+
+    def __init__(self, radius, color):
         self.radius = radius
+        self.color = color
 
     def calculate_diameter(self):
         return self.radius * 2
     
-my_circle = Circle(5)
-print(f'Diameter of My_Circle: {my_circle.calculate_diameter()}')
+    def find_area(self):
+        return (self.radius ** 2) * Circle.PI
+    
+my_circle = Circle(5, "Red")
+print(f'Diameter of My Circle: {my_circle.calculate_diameter()}')
 
 print()
 
+blue_circle = Circle(15, "Blue")
+area = blue_circle.find_area()
+print(f'Area of Blue Circle: {area}')
