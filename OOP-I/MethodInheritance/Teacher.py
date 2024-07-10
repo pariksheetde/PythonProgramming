56eeb5be4aa4a9a5677b4e8bfdadd5968a0e6f18
Pariksheet <deparikshee2@gmail.com>
2024-07-10 14:34:08 +0530

    COMMIT

---
+++ OOP-I/MethodInheritance/Teacher.py
@@ -0,0 +1,32 @@
+class Teacher:
+
+    def __init__(self, full_name, id):
+        self.full_name = full_name
+        self.id = id
+
+    def greetings(self):
+        print(f"Welcome to my class. My name is {self.full_name}")
+
+class ScienceTeacher(Teacher):
+
+    def __init__(self, full_name, id, subject):
+        Teacher.__init__(self, full_name, id)
+        self.subject = subject
+
+    def greetings(self):
+        print(f"I am your science teacher. My name is {self.full_name}")
+
+print()
+print("Let's display Science Teacher's Biodata")
+albert = ScienceTeacher("Albert Einstein", "AE101", ["Physics", "Chemistry"])
+print(f"ID: {albert.id}. {albert.full_name} teaches {albert.subject}.")
+print(f'{albert.greetings()}')
+
+print()
+
+print("Let's display Teacher's Biodata")
+teacher = Teacher("Albert Einstein", "AE101")
+print(f"ID: {teacher.id}. {teacher.full_name}.")
+print(f'{teacher.greetings()}')
+
+print()
