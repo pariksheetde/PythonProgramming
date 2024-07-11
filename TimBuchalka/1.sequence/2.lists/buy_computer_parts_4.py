7e5b2f94151b0e35bc1f941a7bb9d6fb6d51120a
Pariksheet <deparikshee2@gmail.com>
2024-07-11 11:40:31 +0530

    COMMIT

--- TimBuchalka/1.sequence/2.lists/buy_computer_parts_4.py
+++ TimBuchalka/1.sequence/2.lists/buy_computer_parts_4.py
@@ -16,10 +16,15 @@ computer_parts = []
 
 while current_choice != '0':
     if current_choice in another_valid_choice:
-        print(f'Adding {current_choice}')
         index = int(current_choice) - 1
         chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
+        if chosen_part in computer_parts:
+            print(f'Removing: {current_choice}')
+            computer_parts.remove(chosen_part)
+        else:
+            print(f'Adding {current_choice}')
+            computer_parts.append(chosen_part)
+        print(f"Your cart now contains: {chosen_part}")
     else:
         print("Add option from below list")
         for index, part in enumerate(available_parts):
